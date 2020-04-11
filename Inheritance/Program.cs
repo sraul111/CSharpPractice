93a4cc23187e6b5f01302d47a1cbf3ccbf1598ab
Sujit Raul <sraul111@gmail.com>
2020-04-11 10:36:12 +0000

    Inheritance fundas

---
+++ Inheritance/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base b = new Derieved();
+            b.Method1();
+            Derieved d = new Derieved();
+            d.Method1();
+            Console.ReadKey();
+
+        }
+    }
+
+    public class Base
+    {
+        public void Method1()
+        {
+            Console.WriteLine($"Method1 from Base");
+        }
+    }
+
+    public class Derieved : Base
+    {
+        public void Method1()
+        {
+            Console.WriteLine($"Method1 from Derieved");
+        }
+    }
+}
