32bd1aed167537d21a09b9e3caf01e48b536bb07
Sujit Raul <sraul111@gmail.com>
2020-04-11 08:26:18 +0000

    overload example , diff between Int32.Parse and Convert.ToInt32

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+       //program can't declare overload the 2 methods  which differs only on parameters modifier
+        //public void Method1(ref int x) { }
+        //public void Method1(out int x) { x=2; }
+
+        //methods are not overloaded because their return type differs
+        //public string Method2(int x) { return "s"; }
+        //public int Method2(int x) { return  1;}
+
+        static void Main(string[] args)
+        {
+            //the difference between Convert.ToInt32 and Int32.Parse is that Int32.TryParse don't handle 
+            //null argument and throws ArgumentNull Exception.
+            string s = null;
+            Console.WriteLine(Convert.ToInt32(s));
+            Console.WriteLine(Int32.Parse(s));
+            Console.ReadKey();
+        }
+    }
+}
